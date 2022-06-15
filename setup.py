023f21fabf67a5fde78dca43990fa1a9dc1197a5
jgusta <jgustafx@gmail.com>
2022-06-15 09:53:30 -0700

    fix gitignore

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 from setuptools import setup
 
-setup(name="reone",
-      version='1.0',
-      description='Audio Loop Re-one-ing tool',
-      author='Jake Gustafson',
-      author_email='jgustafx@gmail.com',
-      packages=['reone', 'reone.pather']
-      )
+if __name__ == "__main__":
+    setup()
