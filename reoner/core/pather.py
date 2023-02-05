e7b7efb40d2c7740f5cbea51a16567caef7602dc
jgusta <jgustafx@gmail.com>
2023-02-05 13:17:09 -0800

    add clean_filename capability

--- reoner/core/pather.py
+++ reoner/core/pather.py
@@ -1,5 +1,8 @@
 import os
 import os.path
+import unicodedata
+import string
+import re
 
 
 class ImproperlyConfigured(Exception):
@@ -202,3 +205,24 @@ class PatherFile(Pather):
 
     def __str__(self):
         return self.__root__ + '/' + self.__filename__
+
+
+def clean_filename(filename, whitelist=None, replace=''):
+    valid_filename_chars = "-_.() %s%s%s" % (string.ascii_letters, string.digits, whitelist)
+    char_limit = 255
+
+    # replace spaces
+    for r in replace:
+        filename = filename.replace(r, '_')
+
+    # keep only valid ascii chars
+    cleaned_filename = unicodedata.normalize('NFKD', filename).encode('ASCII', 'ignore').decode()
+
+    # keep only whitelisted chars
+    cleaned_filename = ''.join(c for c in cleaned_filename if c in valid_filename_chars)
+    re.sub('_{2,}', '_', filename)
+    if len(cleaned_filename) > char_limit:
+        print(
+            "Warning, filename truncated because it was over {}. Filenames may no longer be unique".format(char_limit))
+
+    return cleaned_filename[:char_limit]
