bc6eecc58d7e05c1fb0ede5e7b06befbd40aa338
jgusta <jgustafx@gmail.com>
2023-02-05 13:18:02 -0800

    Punt main up front

--- reoner/__init__.py
+++ reoner/__init__.py
@@ -1,8 +1,12 @@
-# from .core.reoneablemedia import ReoneableMedia
-# from .cli.menu import Menu
+from .core.reoneablemedia import ReoneableMedia
+from .cli.menu import Menu
+from .cli.startcli import main
 
 # __all__ = [
 #     'from_file'
 #     'ReoneableMedia'
 #     'cli'
 # ]
+
+if __name__ == '__main__':
+    main()
