85721e3685820f1747fb2fdd9d598f57dc898632
jgusta <jgustafx@gmail.com>
2023-11-11 00:35:45 -0800

    export "reone"

--- reoner/__init__.py
+++ reoner/__init__.py
@@ -1,11 +1,12 @@
 from .core.reoneablemedia import ReoneableMedia
-
+from .cli.cli import main as reone
 
 def from_file(filename):
     return ReoneableMedia(filename)
 
 
 __all__ = [
+    'reone',
     'from_file'
     'ReoneableMedia'
 ]
